4ee6b3cab4fa48dc5a37c5ccb78e75f8f941de4e
kosick123 <kousickpaulkareti@gmail.com>
2025-11-18 13:16:42 +0530

    clock

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-import About from './Components/About';
-import Services from './Components/Service';
-import Form from './Components/Form';
+
 
 function App () {
     return(
         <div>
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='About/' element={<About />} />
-                    <Route path='Services/' element={<Services />} />
-                    <Route path='Form/' element={<Form />} />
-                </Routes>
-            </Router>
-
+           <Navbar />
         </div>
 
     )
 }
-export default App;
+export default App;            
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom';
+import {useEffect,useState} from 'react';
 
+function Navbar(){
+  const[time,setTime]=useState(new Date());
 
-function Navbar () {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    },1000);
+ 
+   return () => clearInterval(interval);
+  }, []);
 
-  const navigate=useNavigate();
+  const formatTime = (num) => (num < 10 ? "0" + num : num);
+
   return(
-    <nav className='flex justify-between items-center bg-gray-900 text-white p-5'>
-      <div>
-        <h1 className='text-green-800 text-4xl font-bold'>Portfolio</h1>
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <div className='text-center'>
+        <h1 className='text-5xl text-cyan-400 font-bold mb-4'>Digital Clock</h1>
+        <div className='bg-gray-900 text-green-400 p-6 rounded-xl shadow-2xl text-6xl font-mono tracking-widest'>
+          {formatTime(time.getHours())}:
+          {formatTime(time.getMinutes())}:
+          {formatTime(time.getSeconds())}
+        </div>
       </div>
-      <div className='flex list-none gap-5 items-center'>
-        <button onClick={() => navigate("/")} className='hover:text-blue-700 font-bold'>Home</button>
-        <button onClick={() => navigate("/About")} className='hover:text-blue-700 font-bold'>About</button>
-        <button onClick={() => navigate("/Services")} className='hover:text-blue-700 font-bold'>Services</button>
-        <button onClick={() => navigate("/Form")} className='bg-blue-600 h-7 w-20 text-center rounded-md font-semibold'>Login</button>
-      </div>
-    </nav>
-
+    </div>
   )
 }
-export default Navbar
+export default Navbar;
